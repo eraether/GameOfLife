bb2ef9fe21f5c3b22857d178714b55545f8fc2b0
eraether <eugene.raether@yahoo.com>
2013-02-26 21:17:55 -0500

    First Upload

---
+++ runner.bat
@@ -0,0 +1,36 @@
+cd src
+java -jar RaetherConwayGOL.jar width=100 height=100 type=random livingrules=f,f,t,t,f,f,f,f,f reanimaterules=f,f,f,t,f,f,f,f,f
+pause
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
